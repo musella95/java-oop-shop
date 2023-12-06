0b7697e8cb5c4eaaa04f8941846c4a3151a7700a
musella95 <musellamatteoo@hotmail.com>
2023-12-06 15:58:39 +0100

    base work done

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,23 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+       Prodotto product1 = new Prodotto("scaldacollo","fatto in lana", 22, 24);
+        System.out.println(product1.fullName());
+        System.out.println(product1.basePrice());
+        System.out.println("il prezzo compreso d'iva è: ");
+        System.out.print(product1.ivaPrice());
+        Prodotto product2 = new Prodotto("cappello","fatto in lana merinos", 10, 24);
+        System.out.println(product2.fullName());
+        System.out.println(product2.basePrice());
+        System.out.println("il prezzo compreso d'iva è: ");
+        System.out.print(product2.ivaPrice());
+        Prodotto product3 = new Prodotto("guanti","fatti in pelle di vitello", 30, 24);
+        System.out.println(product3.fullName());
+        System.out.println(product3.basePrice());
+        System.out.println("il prezzo compreso d'iva è: ");
+        System.out.print(product3.ivaPrice());
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,73 @@
-package org.lessons.java.shop;public class Prodotto {
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+// 1 ATTRIBUTI
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private double iva;
+
+// 2 COSTRUCTOR
+
+    public Prodotto(String name, String description, int price, int iva) {
+        int min= 0;
+        int max= 1000;
+        this.code = min + (int)(Math.random() * ((max - min) + 1));
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+// 3 GETTER E SETTER
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+// 4 METODI DI UTILITA'
+
+    public String fullName(){
+        return code +" "+ name;
+    }
+    public String basePrice(){
+        return name + " " + price+"$";
+    }
+    public double ivaPrice(){
+        return iva * price / 100 + price;
+    }
 }
